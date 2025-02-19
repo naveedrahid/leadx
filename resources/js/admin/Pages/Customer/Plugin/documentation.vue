9656d31f3a6bfc9ba9dd370985231eb694c2d691
hammadpixel <hammad.pixelz360@gmail.com>
2025-02-20 00:28:24 +0500

    spam keywords

---
+++ resources/js/admin/Pages/Customer/Plugin/documentation.vue
@@ -0,0 +1,47 @@
+<template>
+    <Head title="Plugin License" />
+    <AppLayout ref="app_layout" :loader="loader">
+        <div class="container-fluid">
+            <Breadcrumb>
+                <template v-slot:title>Plugin Documentation</template>
+                <li class="breadcrumb-item text-muted" aria-current="page">Plugin Documentation</li>
+            </Breadcrumb>
+
+            <div class="row">
+
+                <div class="col-md-6 order-md-1">
+                 <h1>Coming Soon</h1>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import { Head, Link } from '@inertiajs/vue3';
+import AppLayout from '@/admin/Layouts/AppLayout.vue';
+import Breadcrumb from '@/admin/Components/Breadcrumb.vue';
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/dist/sweetalert2.css';
+import moment from 'moment';
+
+export default {
+    components: {
+        Head,
+        Link,
+        AppLayout,
+        Breadcrumb
+    },
+
+
+    methods: {
+    },
+
+
+    mounted() {
+        this.$refs.app_layout.loadScript();
+    }
+}
+</script>
