ee9c351b933019a4d31fee6bd6665794fc1eecda
hammadpixelz <hammad.pixelz360@gmail.com>
2025-04-29 11:04:35 +0500

    header footer fixed on privacy-page

--- resources/js/frontend/Pages/Privacy-policy.vue
+++ resources/js/frontend/Pages/Privacy-policy.vue
@@ -1,18 +1,28 @@
 <template>
-<Head title="Privacy Policy" />
-<MasterLayout :loader="loader">
-    <section class="hero-section overflow-hidden" id="hero">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6 col-md-6">
-                    <div class="hero-content">
-                        <h1 style="text-align: center">Coming Soon</h1>
+    <Head title="Privacy Policy" />
+    <MasterLayout :loader="false">
+        <section class="hero-section overflow-hidden" id="hero">
+            <div class="container" style="height: 200px">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-lg-6 col-md-6 text-center">
+                        <div class="hero-content">
+                            <h1>Coming Soon</h1>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</MasterLayout>
+        </section>
+    </MasterLayout>
 </template>
-<script setup lang="ts">
+
+<script>
+import { Head } from '@inertiajs/vue3';
+import MasterLayout from '@/frontend/Layouts/MasterLayout.vue';
+
+export default {
+    components: {
+        Head,
+        MasterLayout
+    }
+};
 </script>
